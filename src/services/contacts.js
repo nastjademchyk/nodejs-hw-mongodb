bb31943be634f28266916caefd64281000b217c9
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-01-17 16:52:24 +0100

    completed 5 steps from homework

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,26 @@ export const getContactById = async (contactId) => {
   const contact = await contactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await contactsCollection.create(payload);
+  return contact;
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await contactsCollection.findByIdAndDelete({
+    _id: contactId,
+  });
+  return contact;
+};
+
+export const updateContact = async (contactId, payload) => {
+  const updatedContact = await contactsCollection.findOneAndUpdate(
+    {
+      _id: contactId,
+    },
+    { $set: payload },
+    { new: true, runValidators: true }
+  );
+  return updatedContact;
+};
