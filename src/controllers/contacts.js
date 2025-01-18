0d278dee9feb1449923ed13ead88db23f10a79ff
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-01-18 13:00:09 +0100

    corrected mistake in deleteContactController, add next

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -40,7 +40,7 @@ export const createContactController = async (req, res) => {
   });
 };
 
-export const deleteContactController = async (req, res) => {
+export const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await deleteContact(contactId);
   if (!contact) {
