733159b6aaae3bfb4d914594fdea9079b50d5335
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-02-04 09:30:15 +0100

    corrected name of post rout to send-reset-email

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -35,7 +35,7 @@ router.post("/logout", ctrlWrapper(logoutUserController));
 router.post("/refresh", ctrlWrapper(refreshUserSessionController));
 
 router.post(
-  "/request-reset-email",
+  "/send-reset-email",
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController)
 );
