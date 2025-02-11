f38b324cf803ebc1d9d1b7c336dc30a55ed63539
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-02-11 11:43:08 +0100

    created swagger

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import router from "./routers/index.js";
 import { UPLOAD_DIR } from "./constants/index.js";
 
 import cookieParser from "cookie-parser";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 const PORT = Number(getEnvVar("PORT", "3000"));
 
@@ -30,6 +31,8 @@ export const setupServer = () => {
   app.use("/uploads", express.static(UPLOAD_DIR));
 
   app.use(router);
+  app.use("uploads", express.static(UPLOAD_DIR));
+  app.use("/api/docs", swaggerDocs());
 
   app.use("*", notFoundHandler);
 
